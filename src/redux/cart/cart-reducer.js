452669c15e4254cf39d8b493a153ef5cf9a0bbaa
Saad <53213456+kingsaad98@users.noreply.github.com>
2020-05-17 19:58:52 +0100

    adding multiple items in cart

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,3 +1,4 @@
+import { addItemToCart } from "./cart.utils";
 const INITIAL_STATE = {
   isHidden: true,
   cartItems: [],
@@ -13,7 +14,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case "ADD_ITEM":
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
       return state;
