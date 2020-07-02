b18e227bf066272b0c4a08e0922a5b35192598b9
Saad <53213456+kingsaad98@users.noreply.github.com>
2020-07-02 15:41:55 +0100

    added funct to checkout page to disolay cart empty state

--- client/src/pages/checkout-page/checkout-page.component.jsx
+++ client/src/pages/checkout-page/checkout-page.component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
+import { withRouter } from "react-router-dom";
+
 import {
   selectCartItems,
   selectCartItemsTotal,
@@ -11,31 +12,43 @@ import "./checkout-page.styles.scss";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import StripeButton from "../../components/stripe-button/stripe-button.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
 
-const CheckoutPage = ({ cartItems, total }) => {
+const CheckoutPage = ({ cartItems, total, history }) => {
   return (
     <div className="checkout-page">
-      <div className="header">
-        <div className="header-block">Product</div>
-        <div className="header-block">Description</div>
-        <div className="header-block">Quantity</div>
-        <div className="header-block">Price</div>
-        <div className="header-block">Remove</div>
-      </div>
-      <div className="cart-items">
-        {cartItems.map((cartItem) => (
-          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-        ))}
-      </div>
-      <div className="test-warning">
-        *Please use the following test credit card for payments*
-        <br />
-        4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
-      </div>
-      <div className="total">${total}</div>
-      <div className="stripe">
-        <StripeButton price={total}>pay with stripe</StripeButton>
-      </div>
+      {cartItems.length ? (
+        <React.Fragment>
+          <div className="header">
+            <div className="header-block">Product</div>
+            <div className="header-block">Description</div>
+            <div className="header-block">Quantity</div>
+            <div className="header-block">Price</div>
+            <div className="header-block">Remove</div>
+          </div>
+          <div className="cart-items">
+            {cartItems.map((cartItem) => (
+              <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+          </div>
+          <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/22 - CVV: 123
+          </div>
+          <div className="total">${total}</div>
+          <div className="stripe">
+            <StripeButton price={total}>pay with stripe</StripeButton>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="empty-checkout-page">
+          <h2>Oops! your cart is empty ! 0.o</h2>
+          <CustomButton onClick={() => history.push("/shop")}>
+            Let's Shop!
+          </CustomButton>
+        </div>
+      )}
     </div>
   );
 };
@@ -45,4 +58,4 @@ const mapStatetoProps = createStructuredSelector({
   total: selectCartItemsTotal,
 });
 
-export default connect(mapStatetoProps)(CheckoutPage);
+export default connect(mapStatetoProps)(withRouter(CheckoutPage));
